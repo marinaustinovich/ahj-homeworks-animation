fb38eb9eb16adfd6a0bff8e7b504a04cc3083bdb
Marina Ustinovich <marishka0509@mail.ru>
2023-04-19 22:48:50 +0300

    solved the second task about Callback-Chat

---
+++ src/js/Callback-Chat/Modal.js
@@ -0,0 +1,75 @@
+export default class Modal {
+  constructor(container) {
+    if (!(container instanceof HTMLElement)) {
+      throw new Error('container is not HTMLElement');
+    }
+
+    this.container = container;
+    this.error = null;
+    this.drawUi();
+    this.addEvents();
+  }
+
+  drawUi() {
+    this.container.innerHTML = `
+      <div id="modal" class="modal">
+        <div class="modal-wrapper">
+          <div class="modal-content">
+              <h2>Напишите нам</h2>
+              <textarea id="text" class="text-modal" placeholder="Writen your text here"></textarea>
+              <button type="submit" id="submit" class="btn-modal">Отправить</button>
+              <button type="button" class="close-modal">&times;</button>
+          </div>
+          <button type="button" id="open-modal" class="open-modal">Написать</button>
+        </div>
+        <div class="error-container"></div>
+      </div>
+    `;
+  }
+
+  addEvents() {
+    this.modal = this.container.querySelector('#modal');
+    this.textEl = this.container.querySelector('#text');
+    this.submitButton = this.container.querySelector('#submit');
+    this.openButton = this.container.querySelector('#open-modal');
+    this.closeButton = this.container.querySelector('.close-modal');
+    this.errorContainer = this.container.querySelector('.error-container');
+
+    this.submitButton.addEventListener('click', () => this.onSubmit());
+    this.openButton.addEventListener('click', () => this.showModal());
+    this.textEl.addEventListener('click', () => this.clearTextField());
+    this.closeButton.addEventListener('click', () => this.hideModal());
+  }
+
+  onSubmit() {
+    if (this.textEl.value) {
+      this.modal.style.display = 'none';
+    } else {
+      this.showError('Пожалуйста, введите ваш текст');
+    }
+  }
+
+  showModal() {
+    this.modal.querySelector('.modal-content').classList.add('modal-content_visible');
+    this.openButton.classList.add('open-modal_click');
+  }
+
+  hideModal() {
+    this.modal.querySelector('.modal-content').classList.remove('modal-content_visible');
+    this.openButton.classList.remove('open-modal_click');
+  }
+
+  clearTextField() {
+    this.textEl.value = '';
+    this.hideError();
+  }
+
+  showError(message) {
+    this.error.textContent = message;
+    this.error.style.display = 'block';
+  }
+
+  hideError() {
+    this.error.style.display = 'none';
+  }
+}
---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import Modal from './Callback-Chat/Modal';
+
+/* eslint-disable */
+console.log('it works!');
+
+const modal = new Modal(document.getElementById('chat-container'));
+
+
+
